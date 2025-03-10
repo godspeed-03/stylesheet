9218beedb20eedc952e875b437438e4a02df95a0
godspeed-03 <satyam2001anand@gmail.com>
2025-03-10 22:58:17 +0530

    feat changes codebase archt

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-    ...compat.extends(
-        "next/core-web-vitals",
-        "next/typescript"
-        // "plugin:prettier/recommended" // This ensures Prettier's formatting rules are applied
-    ),
-    {
-        ignores: ["node_modules/**", "tailwind.config.ts"], // Flat config format
-    },
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript'
+    // "plugin:prettier/recommended" // This ensures Prettier's formatting rules are applied
+  ),
+  {
+    ignores: ['node_modules/**', 'tailwind.config.ts'], // Flat config format
+  },
 ];
 
 export default eslintConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Home() {
-    return <div className="text-txt">hhhhhhhhhhhhhhhhh</div>;
+  return <div className="text-txt">hhhhhhhhhhhhhhhhh</div>;
 }
