9cb735cbe10e7d72df8efd62c230a31d98e4dadc
godspeed-03 <satyam2001anand@gmail.com>
2025-03-10 21:03:47 +0530

    added lint stage comand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
           src="/next.svg"
           alt="Next.js logo"
           width={180}
-
-
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">             Get started by editing{" "}          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+          <li className="mb-2">
+            {" "}            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
             .
