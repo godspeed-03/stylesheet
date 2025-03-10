8789931cc2497e63fcefa4ad8d2e216147e800a8
godspeed-03 <satyam2001anand@gmail.com>
2025-03-10 21:16:02 +0530

    added lint stage comand

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,12 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends(
-    'next/core-web-vitals',
-    'next/typescript'
+    "next/core-web-vitals",
+    "next/typescript",
     // "plugin:prettier/recommended" // This ensures Prettier's formatting rules are applied
   ),
   {
-    ignores: ['node_modules/**', 'tailwind.config.ts'], // Flat config format
+    ignores: ["node_modules/**", "tailwind.config.ts"], // Flat config format
   },
 ];
 
